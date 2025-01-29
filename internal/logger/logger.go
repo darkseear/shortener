14615454d7165cf5ef3cdd2308b700f42a7b48cc
DarkseearIS <darkseeara@yandex.ru>
2025-01-29 17:54:43 +0300

    fix

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -29,17 +29,57 @@ func Initialize(level string) error {
 	return nil
 }
 
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
 func WhithLogging(h http.Handler) http.HandlerFunc {
 	logFn := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
 		uri := zap.String("uri", r.RequestURI)
 		method := zap.String("method", r.Method)
 
-		h.ServeHTTP(w, r)
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&lw, r)
 		duration := time.Since(start)
 
 		Log.Info("request HTTP",
-			uri, method, zap.Duration("duration", duration),
+			uri,
+			method,
+			zap.Duration("duration", duration),
+			zap.Int("size", responseData.size),
+			zap.Int("status", responseData.status),
 		)
 	}
 
