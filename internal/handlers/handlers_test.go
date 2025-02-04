096908c8884128f17407f67bb8cfbc026b4f760f
darkseear <38041141+darkseear@users.noreply.github.com>
2025-02-05 00:27:05 +0300

    test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/darkseear/shortener/internal/config"
 	"github.com/darkseear/shortener/internal/logger"
 	"github.com/darkseear/shortener/internal/services"
 
@@ -35,10 +36,11 @@ func TestGetURL(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// minURL := storage.RandStringBytes(8)
-			// stor := storage.NewStorageServise().Storage
 			m := services.NewMemory()
-			r := Routers(tt.defURL, m, tt.testFile)
+			config := config.New()
+			fileName := config.MemoryFile
+			bdInfo := config.DatabaseDSN
+			r := Routers(tt.defURL, m, fileName, bdInfo)
 			minURL := m.ShortenURL(tt.url, tt.testFile)
 			request := httptest.NewRequest(http.MethodGet, tt.request+minURL, nil)
 			w := httptest.NewRecorder()
@@ -89,7 +91,10 @@ func TestAddURL(t *testing.T) {
 			request := httptest.NewRequest(http.MethodPost, tt.request, strings.NewReader(tt.urlPlain))
 
 			m := services.NewMemory()
-			r := Routers(tt.defURL, m, tt.testFile)
+			config := config.New()
+			fileName := config.MemoryFile
+			bdInfo := config.DatabaseDSN
+			r := Routers(tt.defURL, m, fileName, bdInfo)
 			w := httptest.NewRecorder()
 			h := logger.WhithLogging(AddURL(*r))
 
