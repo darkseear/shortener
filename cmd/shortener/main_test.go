c4a3159f7b83aa474c1ec15248a0ccee0268cb59
DarkseearIS <darkseeara@yandex.ru>
2025-06-23 16:57:28 +0300

    +

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -16,7 +16,7 @@ func TestApp_Run_GRPCServer(t *testing.T) {
 
 	grpServerAddr := "localhost:9090"
 	testTimeout := 5 * time.Second
-	testUrl := "http://example.com"
+	testURL := "http://example.com"
 
 	conn, err := grpc.NewClient(grpServerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	require.NoError(t, err)
@@ -27,14 +27,14 @@ func TestApp_Run_GRPCServer(t *testing.T) {
 	defer cancel()
 
 	respShort, err := client.Shorten(ctx, &proto.ShortenRequest{
-		Url: testUrl,
+		Url: testURL,
 	})
 	if err != nil {
 		t.Logf("Error resp short: %v", err)
 	}
 	t.Logf("ResponseShort: %v", respShort)
 	respAdd, err := client.AddURL(ctx, &proto.AddURLRequest{
-		Url: testUrl,
+		Url: testURL,
 	})
 
 	if err != nil {
